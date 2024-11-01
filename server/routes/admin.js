7f2b75fa46415bc2f8b864fc00837b628559f1bc
Marksu20 <markjoshuadlcrz@gmail.com>
2024-11-01 21:32:07 +0800

    smol changes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,40 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middleware/checkAuth');
-const { requireAdminPassword } = require('../middleware/requireAdminPassword');
-const { reAuth } = require('../middleware/reAuth');
+const { isAdmin } = require('../middleware/isAdmin');
 const  adminController = require('../controllers/adminController');
 const multer = require('multer');
 const path = require('path');
 const uploads = multer({ dest: 'public/uploads/' });
+
 // admin routes
-
 // GET
-router.get('/pos/admin', isLoggedIn, requireAdminPassword, reAuth, adminController.admin);
-router.get('/pos/admin/admin-login',  isLoggedIn, adminController.adminLogin);
-router.post('/pos/admin/admin-login', isLoggedIn, adminController.adminEntry);
+router.get('/pos/admin/dashboard', isLoggedIn, adminController.dashboard);
 
-router.post('/send-remove-pin-link', isLoggedIn, adminController.sendRemovePIN );
-router.get('/reset-pin/:token', isLoggedIn, adminController.resetPIN );
-router.post('/admin/remove-pin', isLoggedIn, adminController.removePIN );
-
-router.get('/pos/admin/dashboard', isLoggedIn, reAuth, adminController.dashboard);
-
-router.get('/pos/admin/product', isLoggedIn, reAuth, adminController.product);
+router.get('/pos/admin/product', isLoggedIn, adminController.product);
 router.get('/pos/admin/product/:id', isLoggedIn, adminController.viewProduct);
 
-router.get('/pos/admin/category', isLoggedIn, reAuth, adminController.category);
-router.get('/pos/admin/category/:id', isLoggedIn, reAuth, adminController.viewCategory);
+router.get('/pos/admin/category', isLoggedIn, adminController.category);
+router.get('/pos/admin/category/:id', isLoggedIn, adminController.viewCategory);
 
-router.get('/pos/admin/stock', isLoggedIn, reAuth, adminController.stock);
-router.get('/pos/admin/stock/:id', isLoggedIn, reAuth, adminController.viewStock);
+router.get('/pos/admin/stock', isLoggedIn, adminController.stock);
+router.get('/pos/admin/stock/:id', isLoggedIn, adminController.viewStock);
 
-router.get('/pos/admin/receipt', isLoggedIn, reAuth, adminController.receipt);
+router.get('/pos/admin/receipt', isLoggedIn, adminController.receipt);
 
-router.get('/pos/admin/discount', isLoggedIn, reAuth, adminController.discount);
-router.get('/pos/admin/discount/:id', isLoggedIn, reAuth, adminController.viewDiscount);
+router.get('/pos/admin/discount', isLoggedIn, adminController.discount);
+router.get('/pos/admin/discount/:id', isLoggedIn, adminController.viewDiscount);
 
-router.get('/pos/admin/account', isLoggedIn, reAuth, adminController.account);
+router.get('/pos/admin/account', isLoggedIn, adminController.account);
+
+// POST
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+var upload = multer({ storage: storage });
+router.post('/pos/admin/newProduct', upload.single('image'), isLoggedIn, adminController.newProduct);
+
+router.post('/pos/admin/newCategory', isLoggedIn, adminController.newCategory);
+
+router.post('/pos/admin/newStock', isLoggedIn, adminController.newStock);
+
+router.post('/pos/admin/newDiscount', isLoggedIn, adminController.newDiscount);
+
+router.post('/newUser', isLoggedIn, isAdmin, adminController.newUser);
 
 // PUT
 router.put('/pos/admin/product/:id', uploads.single('image'), isLoggedIn, adminController.updateProduct);
@@ -57,24 +68,5 @@ router.delete('/pos/admin/delete-stock/:id', isLoggedIn, adminController.deleteS
 router.delete('/pos/admin/delete-discount/:id', isLoggedIn, adminController.deleteDiscount);
 
 router.delete('/delete-receipt/:id', isLoggedIn, adminController.deleteReceipt);
-
-
-// POST
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-var upload = multer({ storage: storage });
-router.post('/pos/admin/newProduct', upload.single('image'), isLoggedIn, adminController.newProduct);
-
-router.post('/pos/admin/newCategory', isLoggedIn, adminController.newCategory);
-
-router.post('/pos/admin/newStock', isLoggedIn, adminController.newStock);
-
-router.post('/pos/admin/newDiscount', isLoggedIn, adminController.newDiscount);
 
 module.exports = router;
--- server/routes/kiosk.js
+++ server/routes/kiosk.js
@@ -11,6 +11,4 @@ router.post('/kiosk/orders', kioskController.orders);
 
 router.get('/kiosk/orders/generate-order-number', kioskController.generateOrderNumber);
 
-
-
 module.exports = router;
