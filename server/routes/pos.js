ca23467c1fcac6c21444bdf875b3530cdfcaf18a
Marksu20 <markjoshuadlcrz@gmail.com>
2024-12-28 17:51:06 +0800

    check role for routes

--- server/routes/pos.js
+++ server/routes/pos.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middleware/checkAuth');
+const { checkRole } = require('../middleware/checkRole');
 const posController = require('../controllers/posController');
 
 // pos routes
 
-router.get('/pos', isLoggedIn, posController.pos);
-router.get('/pos/order-notif', isLoggedIn, posController.orderNotif);
+router.get('/pos', isLoggedIn, checkRole(['admin', 'sub-admin', 'staff']), posController.pos);
+router.get('/pos/order-notif', checkRole(['admin', 'sub-admin', 'staff']), isLoggedIn, posController.orderNotif);
 
-router.get('/pos/order', isLoggedIn, posController.order);
-router.get('/pos/order/order-count', isLoggedIn, posController.orderCount);
+router.get('/pos/order', isLoggedIn, checkRole(['admin', 'sub-admin', 'staff']), posController.order);
+router.get('/pos/order/order-count', checkRole(['admin', 'sub-admin', 'staff']), isLoggedIn, posController.orderCount);
 
-router.get('/pos/receipt', isLoggedIn, posController.receipt);
+router.get('/pos/receipt', isLoggedIn, checkRole(['admin', 'sub-admin', 'staff']), posController.receipt);
 
 router.post('/pos/confirm-payment', isLoggedIn, posController.confirmPayment);
 
-router.get('/orders/:id', isLoggedIn, posController.viewOrder);
-router.put('/orders/:id', isLoggedIn, posController.updateOrder);
-router.put('/orders/:id/served', isLoggedIn, posController.served);
+router.get('/orders/:id', isLoggedIn, checkRole(['admin', 'sub-admin', 'staff']), posController.viewOrder);
+router.put('/orders/:id', isLoggedIn, checkRole(['admin', 'sub-admin', 'staff']), posController.updateOrder);
+router.put('/orders/:id/served', checkRole(['admin', 'sub-admin', 'staff']), isLoggedIn, posController.served);
 
 // DELETE
 router.delete('/delete-order/:id', isLoggedIn, posController.deleteOrder)
