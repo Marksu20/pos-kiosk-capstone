d13a2d479227c77b23c76a8a794476cc925153c9
Marksu20 <markjoshuadlcrz@gmail.com>
2025-05-05 17:40:09 +0800

    debug

--- server/controllers/kioskController.js
+++ server/controllers/kioskController.js
@@ -295,11 +295,11 @@ exports.createPaypalOrder = async (req, res) => {
           {
             amount: {
               currency_code: 'PHP',
-              value: (req.body.amount).toFixed(2),
+              value: totalAmount.toFixed(2),
               breakdown: {
                 item_total: {
                   currency_code: 'PHP',
-                  value: (req.body.amount).toFixed(2),
+                  value: totalAmount.toFixed(2),
                 },
               },
             },
