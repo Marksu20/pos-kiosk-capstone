76fb53582adaf35ad62584d1e8cf82a70b051284
Marksu20 <markjoshuadlcrz@gmail.com>
2025-05-06 19:34:36 +0800

    multer debug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const connectDB = require('./server/config/db');
 const session = require('express-session');
 const passport = require('passport'); 
 const MongoStore = require('connect-mongo');
-const multer = require('multer');
+// const multer = require('multer');
 const socketIO = require('socket.io');
 const flash = require('connect-flash');
 
--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,15 +1,21 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('./cloudinary');
+const cloudinary = require('cloudinary').v2;
 
-const storage = new CloudinaryStorage({ 
-    cloudinary,
-    params: {
-        folder: 'products',
-        allowed_formats: ['jpg', 'png', 'jpeg'],
-    },
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
 });
 
-const upload = multer({ storage });
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'products',
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
+
+const upload = multer({ storage: storage });
 
 module.exports = upload;
