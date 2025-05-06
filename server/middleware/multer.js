8f4ce36e990b0704a2bd04392bed053ea95655df
Marksu20 <markjoshuadlcrz@gmail.com>
2025-05-06 19:53:15 +0800

    cloud debug

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -3,9 +3,9 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'KUYA_KOKA',
-  api_key: '171817433729255',
-  api_secret: '-nGwiKscovi-phqXcVHFQIPXVGs'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
